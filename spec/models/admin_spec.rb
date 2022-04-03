0ae318e4443fb03761cba51da2a3a41db97a09c3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-03 00:58:45 -0300

    admin test

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Admin, type: :model do
     expect(admin.email).to eql 'test@mail.com'
     expect(admin).to be_valid
   end
+
+  it 'admin is not valid' do
+    admin = Admin.new(email: 'test@mail.com')
+
+    expect(admin).to_not be_valid
+  end
 end
