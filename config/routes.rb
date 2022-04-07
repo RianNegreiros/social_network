3768a05d0bf05d3f73cb886271e2c1b6b7def1b7
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-07 15:39:39 -0300

    admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
 
   namespace :admin do
-    root to: 'admin/home#index'
+    root to: 'home#index'
+    resources :admins
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
