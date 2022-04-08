0a477946b80c5c3eafaa3c9f756206ecfa8762c6
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-08 11:55:39 -0300

    new method

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -2,4 +2,14 @@ class Admin::AdminsController < AdminController
   def index
     @admins = Admin.order(id: :desc)
   end
+
+  def new
+    @admin = Admin.new
+  end
+
+  private
+
+  def form_params
+    params.require(:admin).permit(:name, :email, :password, :password_confirmation)
+  end
 end
