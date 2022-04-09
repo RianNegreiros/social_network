75ff19342acb9de63cc9c91f639111e87b895694
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-09 15:38:16 -0300

    user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'user is valid' do
+    @user = User.new(
+      name: 'test_name', 
+      email: 'test@mail.com', 
+      phone: '9090909090',
+      avatar: fixture_file_upload(Rails.join('spec', 'support', 'test-img.png'), 'image/png'),
+      password: 'testpassword'
+    )
+
+    expect(user).to be_valid
+  end
+
+  it 'user is not valid' do
+    @user = User.new(
+      name: 'test_name', 
+      email: 'test@mail.com', 
+      phone: '9090909090',
+      password: 'testpassword'
+    )
+
+    expect(user).to_not be_valid
+  end
 end
