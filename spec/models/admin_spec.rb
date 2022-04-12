909047dc05243f4d3124aa26b668fff7f6a8efc0
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-12 19:38:31 -0300

    fix typos

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   it 'admin is valid' do
-    admin = Admin.new(name: 'admin_name', email: 'test@mail.com', password: 'testpassword')
+    admin = Admin.new(email: 'test@mail.com', password: 'testpassword')
 
     expect(admin.email).to eql 'test@mail.com'
     expect(admin).to be_valid
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe User, type: :model do
     @user = User.new(
       name: 'test_name', 
       email: 'test@mail.com', 
-      phone: '9090909090',
-      avatar: fixture_file_upload(Rails.join('spec', 'support', 'test-img.png'), 'image/png'),
+      phone_number: '9090909090',
+      avatar: fixture_file_upload(Rails.root.join('spec', 'support', 'test_img.png'), 'image/png'),
       password: 'testpassword'
     )
 
-    expect(user).to be_valid
+    expect(@user).to be_valid
   end
 
   it 'user is not valid' do
     @user = User.new(
       name: 'test_name', 
       email: 'test@mail.com', 
-      phone: '9090909090',
+      phone_number: '9090909090',
       password: 'testpassword'
     )
 
-    expect(user).to_not be_valid
+    expect(@user).to_not be_valid
   end
 end
