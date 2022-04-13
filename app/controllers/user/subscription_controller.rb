de2feb676d3fecf2f7758d21f1c9cdc28e4dc441
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-13 10:34:33 -0300

    subscription controller

---
+++ app/controllers/user/subscription_controller.rb
@@ -0,0 +1,10 @@
+class User::SubscriptionController < UserController
+
+  def follow
+    user_to_follow = User.find_by(id: params[:id])
+
+    Subscription.create(followed_by: current_user, followed: user_to_follow)
+
+    redirect_to user_potencial_to_follow
+  end
+end
