fa34c1d384b878795db718dbf1cc42c547d8d48e
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-14 18:39:28 -0300

    comment model tests

---
+++ spec/factories/comment.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :comment do
+    body { 
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  end
+end
