5a7607b875cbcb97f3802d81a039db575b3046f4
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-14 18:43:38 -0300

    add create and destroy comments

--- app/controllers/user/timeline_controller.rb
+++ app/controllers/user/timeline_controller.rb
@@ -2,5 +2,6 @@ class User::TimelineController < UserController
 
   def index
     @posts = Post.where(user_id: current_user.following.pluck(:id)).order(created_at: :desc)
+    @comments = Comment.new
   end
 end
