c6e07af96f8bdbc93fe5f6531d1acf307e14ec67
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-14 13:23:28 -0300

    like toggle

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,5 +1,5 @@
 class User::PostsController < UserController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, only: :like_toggle
 
   def create
     f_params = form_params.merge(user: current_user)
@@ -14,10 +14,24 @@ class User::PostsController < UserController
   end
 
   def destroy
-    post = Post.find(params[:id])
+    post = current_user.posts.find(params[:id])
     post.destroy
 
     redirect_to user_profile_path
+  end
+
+  def like_toggle
+    post = Post.find(params[:id])
+    like = Like.find_by(user: current_user, post: post)
+
+    if like.present?
+      like.destroy
+      respond_to { |format| format.json { render json: { linked: false, likesCount: post.likes.count } } }
+    else
+      Like.create(user: current_user, post: post)
+      respond_to { |format| format.json { render json: { linked: true, likesCount: post.likes.count } } }
+
+    end
   end
 
   private
